e92bd535f11cab89044eb61f4e0f02f9e9df964f
swetha-2321 <76618213+swetha-2321@users.noreply.github.com>
2023-11-14 12:05:07 +0530

    Add files via upload

---
+++ WordCounter/src/KeyAdapter.java
@@ -0,0 +1,14 @@
+
+import java.awt.event.KeyListener;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author swetha
+ */
+
+
